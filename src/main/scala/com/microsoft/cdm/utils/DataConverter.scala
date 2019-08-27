03d808e6a8b3d43de6b005757259e38308b811c1
Deepanshu <deepanshu2017@gmail.com>
2019-08-27 12:39:34 -0700

    Fixed String issue and converted DateTime to TimeZone
    Signed-off-by: Deepanshu <deepanshu2017@gmail.com>

--- src/main/scala/com/microsoft/cdm/utils/DataConverter.scala
+++ src/main/scala/com/microsoft/cdm/utils/DataConverter.scala
@@ -1,9 +1,12 @@
 package com.microsoft.cdm.utils
 
 import java.text.SimpleDateFormat
+import java.util.{Locale, TimeZone}
 
 import org.apache.commons.lang.time.DateUtils
+import org.apache.spark.sql.catalyst.util.{DateTimeUtils, TimestampFormatter}
 import org.apache.spark.sql.types._
+import org.apache.spark.unsafe.types.UTF8String
 
 /**
   * Converts between CSV/CDM data and Spark data tpyes.
@@ -15,19 +18,24 @@ class DataConverter(val dateFormats: Array[String] = Constants.DATE_FORMATS,
 
   val dateFormatter = new SimpleDateFormat(outputDateFormat)
 
+  private val timestampFormatter = TimestampFormatter("MM/dd/yyyy", TimeZone.getTimeZone("UTC"), Locale.US
+
+  var format = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a", Locale.US)
+  var format2 = new SimpleDateFormat("MM/dd/yyyy")
+
   val toSparkType: Map[CDMDataType.Value, DataType] = Map(
     CDMDataType.int64 -> LongType,
-    CDMDataType.dateTime -> DateType,
+    CDMDataType.dateTime -> TimestampType,
     CDMDataType.string -> StringType,
     CDMDataType.double -> DoubleType,
     CDMDataType.decimal -> DecimalType(Constants.DECIMAL_PRECISION,0),
     CDMDataType.boolean -> BooleanType,
-    CDMDataType.dateTimeOffset -> DateType
+    CDMDataType.dateTimeOffset -> TimestampType
   )
 
   val toCdmType: Map[DataType, CDMDataType.Value] = Map(
     LongType -> CDMDataType.int64,
-    DateType -> CDMDataType.dateTime,
+    TimestampType -> CDMDataType.dateTime,
     StringType -> CDMDataType.string,
     DoubleType -> CDMDataType.double,
     DecimalType(Constants.DECIMAL_PRECISION,0) -> CDMDataType.decimal,
@@ -36,18 +44,19 @@ class DataConverter(val dateFormats: Array[String] = Constants.DATE_FORMATS,
 
   val jsonToData: Map[DataType, String => Any] = Map(
     LongType -> (x => x.toLong),
-    StringType -> (x => x),
+    StringType -> (x => UTF8String.fromString(x)),
     DoubleType -> (x => x.toDouble),
     DecimalType(Constants.DECIMAL_PRECISION,0) -> (x => BigDecimal(x, Constants.MATH_CONTEXT)),
     BooleanType -> (x => x.toBoolean),
-    DateType -> (x => new java.sql.Date(DateUtils.parseDate(x, dateFormats).getTime))
+//    TimestampType -> (x => DateUtils.parseDate(x, dateFormats).getTime)
+    TimestampType -> (x => timestampFormatter.parse(format2.format(format.parse(x))))
   )
 
   def dataToString(data: Any, dataType: DataType): String = {
     if(data == null) {
       null
     }
-    else if(dataType == DateType) {
+    else if(dataType == TimestampType) {
       dateFormatter.format(data)
     }
     else {
